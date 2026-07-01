cc563da18ea61a8e8341dc9357a00bd113459df7
Fadi_Dev <fadi.aldawoud@aeonpaymenttechnologies.com>
2026-07-01 17:18:44 +0300

    fix file name

--- backend/manage.py
+++ backend/manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    # Your settings.py is in the same directory as manage.py
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
